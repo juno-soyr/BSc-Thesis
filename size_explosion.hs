fe33eb2f2f5e279d8c0d669fb7ee46a6a6bb375c
Haileselassie Gaspar <h.gaspar.marraco@student.vu.nl>
2025-02-25 02:53:51 +0100

    Initial state of a lambda evaluator

--- size_explosion.hs
+++ size_explosion.hs
@@ -1,1 +1,11 @@
 type VarName = String
+
+data Term =
+  TmVar VarName
+  | TmAbs VarName Term
+  | TmApp Term Term deriving (Eq, Show)
+
+eval :: Term -> Term
+eval (TmApp (TmAbs x t12) v2@(TmAbs _ _))
+eval (TmApp t1 t2)
+eval _ = error "No rule applies"
