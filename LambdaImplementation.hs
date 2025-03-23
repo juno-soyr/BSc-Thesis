6bb7ea0a6a94511552b69c4e8759d7ae696c42fb
haile <haile>
2025-03-23 20:44:58 +0100

    Progress! A functional implementation of the free variable set

--- LambdaImplementation.hs
+++ LambdaImplementation.hs
@@ -6,15 +6,30 @@ data Term =
   | Var VarName
  deriving (Eq, Show)
 
+-- Look up sources
+-- Based on femke's notes on lambda calculus
+
+-- Remove a variable from a list of variables
 removeVar :: VarName -> [VarName] -> [VarName]
 removeVar _ [] = []
 removeVar x (y:ys)
   |x == y = removeVar x ys
   |otherwise = y : removeVar x ys
 
+-- remove duplicate free variables
+rmDup :: [VarName] -> [VarName]
+rmDup [] = []
+rmDup (x:xs)
+  | x `elem` xs = rmDup xs
+  | otherwise = x : rmDup xs
+
+
+-- Get the set of free variables -> Make sure no duplicates(Proper set)
 freeVar :: Term -> [VarName]
 freeVar (Var a) = [a]
-freeVar (App a b) = freeVar a ++ freeVar b
+freeVar (App a b) = rmDup (freeVar a ++ freeVar b)
 freeVar (Abs x a) = removeVar x (freeVar a)
+-- Get the set of bound variables -> TODO
+
 -- lo_red :: Term -> [Term]
 -- lo_red TmAbs VarName Term =
