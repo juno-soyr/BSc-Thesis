51c835a2dc5e90476034a96efae74f9a998310ee
haile <haile>
2025-05-21 23:57:03 +0200

    Report start

--- Report/.auctex-auto/main.el
+++ Report/.auctex-auto/main.el
@@ -6,11 +6,13 @@
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("article" "12pt")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
-                     '(("asmath" "")))
+                     '(("amsmath" "") ("cite" "") ("geometry" "a4paper" "total={6in, 9in}")))
    (TeX-run-style-hooks
     "latex2e"
     "article"
     "art12"
-    "asmath"))
+    "amsmath"
+    "cite"
+    "geometry"))
  :latex)
 
