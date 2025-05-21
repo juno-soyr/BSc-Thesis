21b89386f52b4074695d4b4895cb5e9669e550a2
haile <haile>
2025-05-21 22:59:32 +0200

    Report

--- Report/.auctex-auto/main.el
+++ Report/.auctex-auto/main.el
@@ -5,9 +5,12 @@
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("article" "12pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("asmath" "")))
    (TeX-run-style-hooks
     "latex2e"
     "article"
-    "art12"))
+    "art12"
+    "asmath"))
  :latex)
 
