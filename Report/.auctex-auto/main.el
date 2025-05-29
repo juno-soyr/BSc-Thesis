3a025e6408e2543b6918783855e978879fd91eda
haile <haile>
2025-05-29 22:00:01 +0200

    Sketch of the thesis

--- Report/.auctex-auto/main.el
+++ Report/.auctex-auto/main.el
@@ -6,12 +6,13 @@
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("article" "12pt")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
-                     '(("amsmath" "") ("cite" "") ("geometry" "a4paper" "total={6in, 9in}")))
+                     '(("amsmath" "") ("amssymb" "") ("cite" "") ("geometry" "a4paper" "total={6in, 9in}")))
    (TeX-run-style-hooks
     "latex2e"
     "article"
     "art12"
     "amsmath"
+    "amssymb"
     "cite"
     "geometry"))
  :latex)
