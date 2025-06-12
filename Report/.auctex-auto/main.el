15cd447fa856136a798f456a9603325491b31534
haile <haile>
2025-06-13 00:15:45 +0200

    Update

--- Report/.auctex-auto/main.el
+++ Report/.auctex-auto/main.el
@@ -6,11 +6,11 @@
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("article" "12pt")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
-                     '(("amsmath" "") ("amssymb" "") ("inputenc" "utf8") ("csquotes" "") ("biblatex" "backend=biber") ("geometry" "a4paper" "total={6in, 9in}")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+                     '(("amsmath" "") ("amssymb" "") ("inputenc" "utf8") ("csquotes" "") ("tikz" "") ("biblatex" "backend=biber") ("geometry" "a4paper" "total={6in, 9in}")))
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
    (TeX-run-style-hooks
     "latex2e"
     "article"
@@ -19,6 +19,7 @@
     "amssymb"
     "inputenc"
     "csquotes"
+    "tikz"
     "biblatex"
     "geometry")
    (LaTeX-add-labels
