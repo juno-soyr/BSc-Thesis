d9425fee3d24618d1d6f52b33b22aba74a2e5c1c
haile <haile>
2025-06-28 12:12:04 +0200

    Changes on thesis

--- Report/.auctex-auto/main.el
+++ Report/.auctex-auto/main.el
@@ -6,12 +6,17 @@
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("article" "12pt")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
-                     '(("amsmath" "") ("amssymb" "") ("inputenc" "utf8") ("csquotes" "") ("tikz" "") ("biblatex" "backend=biber") ("geometry" "a4paper" "total={6in, 9in}")))
+                     '(("amsmath" "") ("amsthm" "") ("amssymb" "") ("inputenc" "utf8") ("csquotes" "") ("tikz" "") ("biblatex" "backend=biber") ("geometry" "a4paper" "total={6in, 9in}")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
    (TeX-run-style-hooks
     "latex2e"
     "article"
     "art12"
     "amsmath"
+    "amsthm"
     "amssymb"
     "inputenc"
     "csquotes"
@@ -19,10 +24,13 @@
     "biblatex"
     "geometry")
    (LaTeX-add-labels
-    "abstract"
     "theoretical-background"
     "intro-lambda"
     "reduction")
-   (LaTeX-add-bibliographies))
+   (LaTeX-add-bibliographies)
+   (LaTeX-add-amsthm-newtheorems
+    "theorem"
+    "corollary"
+    "lemma"))
  :latex)
 
