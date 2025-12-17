d3511bc6dbcc6b80c9135bed6ab77771024141cd
soyr <h.gaspar@proton.me>
2025-12-18 00:42:29 +0100

    Almost done

--- Report/final/.auctex-auto/final.el
+++ Report/final/.auctex-auto/final.el
@@ -6,11 +6,11 @@
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("article" "11pt")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
-                     '(("graphicx" "") ("amsmath" "") ("amsthm" "") ("amssymb" "") ("inputenc" "utf8") ("csquotes" "") ("tikz" "") ("biblatex" "backend=biber") ("geometry" "a4paper" "total={6in, 9in}")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+                     '(("tikz" "") ("graphicx" "") ("amsmath" "") ("amsthm" "") ("amssymb" "") ("inputenc" "utf8") ("csquotes" "") ("biblatex" "backend=biber") ("geometry" "a4paper" "total={6in, 9in}")))
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
    (TeX-run-style-hooks
     "latex2e"
     "article"
@@ -21,7 +21,6 @@
     "amssymb"
     "inputenc"
     "csquotes"
-    "tikz"
     "biblatex"
     "geometry")
    (LaTeX-add-labels
@@ -29,10 +28,10 @@
     "reduction"
     "lemma1"
     "lou")
-   (LaTeX-add-bibliographies)
-   (LaTeX-add-amsthm-newtheorems
+   (LaTeX-add-environments
     "theorem"
     "corollary"
-    "lemma"))
+    "lemma")
+   (LaTeX-add-bibliographies))
  :latex)
 
